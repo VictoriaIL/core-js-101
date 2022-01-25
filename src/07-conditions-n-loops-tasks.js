689da532603e176722330c58d619bdb984d02a56
Victoria Ilyina <47503735+VictoriaIL@users.noreply.github.com>
2022-01-25 03:06:03 +0300

    feat: add the solution

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -403,8 +403,24 @@ function getCommonDirectoryPath(/* pathes */) {
  *                         [ 6 ]]
  *
  */
-function getMatrixProduct(/* m1, m2 */) {
-  throw new Error('Not implemented');
+function getMatrixProduct(m1, m2) {
+  const width = m1.length;
+  const height = m2[0].length;
+
+  const resultMatrix = Array(height)
+    .fill(0).reduce((m) => {
+      m.push(Array(width).fill(0));
+      return m;
+    }, []);
+
+  for (let i = 0; i < m1.length; i += 1) {
+    for (let j = 0; j < m2[0].length; j += 1) {
+      let res = 0;
+      for (let k = 0; k < m1[0].length; k += 1) res += m1[i][k] * m2[k][j];
+      resultMatrix[i][j] = res;
+    }
+  }
+  return resultMatrix;
 }
 
 
